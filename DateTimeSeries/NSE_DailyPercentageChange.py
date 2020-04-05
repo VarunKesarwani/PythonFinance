64c05444d7bf4d37256932e19f7e044bdae222a9
VarunKesarwani <varungupta.v@outlook.com>
2020-04-05 12:58:53 +0530

    final changes

---
+++ DateTimeSeries/NSE_DailyPercentageChange.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import pandas_datareader.data as web
+import datetime as dt
+from dateutil.relativedelta import relativedelta
+import pyodbc
+from sqlalchemy import create_engine
+from time import sleep
+
+str_start = dt.datetime.today().date().strftime('%Y-%m-%d')
+end= dt.datetime.today().date() + relativedelta(days=-3)
+str_end = end 
+
+conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
+                      'Server=.\SQLEXPRESS;'
+                      'Database=ShareData;'
+                      'UID=sa; PWD=varun@17;')
+cursor = conn.cursor()
+query = "Select top 10 [CompanyId], [Date],[Close] from CompanyDailyData (nolock) where [Date] between '{}'  and '{}'".format(str_end,str_start)
+print(query)
+df = pd.read_sql(query, conn)
+
+df['returns'] = df['Close'].pct_change(1)
+print(df.head())
+for col in df.iterrows():
+    print(col['returns'])
+
+#print(df.head(20))
+
